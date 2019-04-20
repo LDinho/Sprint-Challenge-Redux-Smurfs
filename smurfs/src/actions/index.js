87ab92acf3341b59e246d5dd9f9081fe8c672cb8
LD <LDinho@users.noreply.github.com>
2019-04-20 12:57:08 -0400

    set up actions & reducers

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,8 +1,3 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
-
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +8,31 @@
    U - updateSmurf
    D - deleteSmurf
 */
+import axios from 'axios';
+
+export const FETCHING_SMURFS = "fetching_smurfs";
+export const FETCHING_SMURFS_SUCCESS = "fetching_smurfs_success";
+export const FETCHING_SMURFS_FAILURE = "fetching_smurfs_failure";
+
+export const getSmurfs = () => (dispatch) => {
+  dispatch({ type: FETCHING_SMURFS });
+
+  const request = axios.get(`http://localhost:3333/smurfs`);
+
+  return request.then(({ data }) => {
+    console.log('AXIOS-GET-RES:', data);
+    dispatch({type: FETCHING_SMURFS_SUCCESS, payload: data.results});
+  })
+    .catch(err => {
+      dispatch({type: FETCHING_SMURFS_FAILURE, error: err});
+    });
+};
+
+// export const ADDING_SMURF = "adding_smurf";
+// export const ADDING_SMURF_SUCCESS = "adding_smurf_success";
+//
+// export const addSmurf = () => (dispatch) => {
+//   dispatch({ type: ADDING_SMURF });
+//
+// }
+
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,9 +1,4 @@
 /*
-  Be sure to import in all of the action types from `../actions`
-*/
-
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
  {
    smurfs: [],
    fetchingSmurfs: false
@@ -14,10 +9,40 @@
  }
 */
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
+import {
+  FETCHING_SMURFS,
+  FETCHING_SMURFS_SUCCESS,
+  FETCHING_SMURFS_FAILURE,
+} from "../actions";
+
+const initialState = {
+  smurfs: [],
+  isLoading: false,
+  error: '',
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHING_SMURFS:
+      console.log('STATE:', state)
+      return {
+        ...state,
+        error: '',
+        isLoading: true,
+      };
+    case FETCHING_SMURFS_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        characters: action.payload,
+      };
+    case FETCHING_SMURFS_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
+      };
+    default:
+      return state;
+  }
+};
